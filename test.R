b9e69a53c7d1be3a24ca858206e443029ef5e30d
emily vagiona <emilyvagiona@gmail.com>
2018-10-28 11:10:20 +0200

    foti kalhmera, kollhsa...thelw na dimiourghsw subset gia na krathsw ta "FPKM" columns. otan kanw to subset "mydata" kai krataw mono to "gene_id", trexei kanonika. otan vazw "FPKM", pairw ayto "Error in .f(.x[[i]], ...) : object 'FPKM.NIL38_F12_R1_E' not found"

--- test.R
+++ test.R
@@ -1,3 +1,13 @@
-# This is a test script
-#hi
-#hi2
+library(tidyverse)
+library(dplyr)
+
+pdata<-E_MTAB_4222 #name the table ?????
+dim(pdata) #length of row names
+str(pdata) #display the internal structure
+head(pdata) #returns the first parts of the table
+tail(pdata) #returns the last parts of the table
+summary(pdata) #summary of data
+names(pdata) #names of rows of the table
+mydata<-select(pdata,gene_id)#create subset
+mydata
+mydata<-select(pdata,gene_id,FPKM.NIL38_F12_R1_E-MTAB-4222)#????????????????
